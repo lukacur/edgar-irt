580223a9780b67e842eda0267ecaab6df1e098a8
lukacur <92858303+lukacur@users.noreply.github.com>
2023-12-16 20:14:48 +0100

    Update IIRTRunner
    Updated the batch type from IBatch to AbstractBatch

--- src/ApplicationModel/Runner/IIRTRunner.ts
+++ src/ApplicationModel/Runner/IIRTRunner.ts
@@ -1,9 +1,9 @@
 import { IItem } from "../../IRT/Item/IItem.js";
 import { AbstractLogisticFunctionParams } from "../../IRT/LogisticFunction/LogisticFunctionTypes.js";
-import { IBatch } from "../Batch/IBatch.js";
+import { AbstractBatch } from "../Batch/AbstractBatch.js";
 
 export interface IIRTRunner<TItem extends IItem> {
-    loadItemBatch(itemBatch: IBatch<TItem>): Promise<void>;
+    loadItemBatch(itemBatch: AbstractBatch<TItem>): Promise<void>;
     addItem(item: TItem): Promise<void>;
     batchCalculateParams(): Map<TItem, AbstractLogisticFunctionParams>;
 }
