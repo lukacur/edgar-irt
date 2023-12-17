a0cace6f5affefebd05a4043ced1b0e59ad62c5a
lukacur <92858303+lukacur@users.noreply.github.com>
2023-12-17 21:44:27 +0100

    Added new configuration rules to IRTService
    Made it that only 1 instance of the IRTService can be started at a time

--- src/IRTService.ts
+++ src/IRTService.ts
@@ -15,6 +15,8 @@ export type ShutdownHookState = "PRE_SHUTDOWN" | "SHUTDOWN" | "POST_SHUTDOWN";
 export type ShutdownHook = (state: ShutdownHookState) => Promise<void>;
 
 class ConfiguredIRTService<TItem extends IItem> {
+    private wasShutdown: boolean = false;
+
     private readonly shutdownHooks: ShutdownHook[] = [];
 
     constructor(
@@ -23,22 +25,47 @@ class ConfiguredIRTService<TItem extends IItem> {
     ) {}
 
     public initialize(): IRTServiceInitializer<TItem> {
+        if (this.wasShutdown) {
+            throw new IRTServiceConfigurationException(
+                "Instance of this service was shutdown. Create a new instance using the IRTService.configure method"
+            );
+        }
+
         return new IRTServiceInitializer(this);
     }
 
     public addShutdownHook(hook: ShutdownHook): ConfiguredIRTService<TItem> {
+        if (this.wasShutdown) {
+            throw new IRTServiceConfigurationException(
+                "Instance of this service was shutdown. Create a new instance using the IRTService.configure method"
+            );
+        }
+
         this.shutdownHooks.push(hook);
         return this;
     }
 
     public startIRTService(): ConfiguredIRTService<TItem> {
+        if (this.wasShutdown) {
+            throw new IRTServiceConfigurationException(
+                "Instance of this service was shutdown. Create a new instance using the IRTService.configure method"
+            );
+        }
+
         return this;
     }
 
     public async shutdownIRTService(forceAfterSeconds?: number): Promise<void> {
-        await Promise.all(this.shutdownHooks.map(sh => sh("PRE_SHUTDOWN")));
+        if (this.wasShutdown) {
+            return;
+        }
         
+        await Promise.all(this.shutdownHooks.map(sh => sh("PRE_SHUTDOWN")));
+
         await Promise.all(this.shutdownHooks.map(sh => sh("POST_SHUTDOWN")));
+
+        IRTService.shutdownUnconfigure();
+        this.wasShutdown = true;
     }
 }
 
@@ -63,14 +90,34 @@ class IRTServiceConfigurer<TItem extends IItem> {
             );
         }
 
+        IRTService.setConfigured();
+
         return new ConfiguredIRTService<TItem>(this.driver, this.statisticsProcessor);
     }
 }
 
 export class IRTService {
+    private static configured: boolean = false;
+
     private constructor() {}
 
+    static setConfigured(): void {
+        if (IRTService.configured) {
+            throw new IRTServiceConfigurationException("Service was already configured");
+        }
+
+        IRTService.configured = true;
+    }
+
+    static shutdownUnconfigure(): void {
+        IRTService.configured = false;
+    }
+
     public static configure<TItem extends IItem>(): IRTServiceConfigurer<TItem> {
+        if (IRTService.configured) {
+            throw new IRTServiceConfigurationException("Service was already configured");
+        }
+
         return new IRTServiceConfigurer();
     }
 }
