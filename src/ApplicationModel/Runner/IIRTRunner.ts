5fd4984aabd3551483b328f2e0d92a969f405224
lukacur <92858303+lukacur@users.noreply.github.com>
2023-12-18 12:32:28 +0100

    Removed unused IIRTRunner interface
    Functionallity moved to a singleton MasterRunner class

--- src/ApplicationModel/Runner/IIRTRunner.ts
+++
@@ -1,9 +0,0 @@
-import { IItem } from "../../IRT/Item/IItem.js";
-import { AbstractLogisticFunctionParams } from "../../IRT/LogisticFunction/LogisticFunctionTypes.js";
-import { AbstractBatch } from "../Batch/AbstractBatch.js";
-
-export interface IIRTRunner<TItem extends IItem> {
-    loadItemBatch(itemBatch: AbstractBatch<TItem>): Promise<void>;
-    addItem(item: TItem): Promise<void>;
-    batchCalculateParams(): Map<TItem, AbstractLogisticFunctionParams>;
-}
