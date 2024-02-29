03c87a274c81548f35bfea97ac47b2ae18ff05a2
lukacur <92858303+lukacur@users.noreply.github.com>
2024-02-29 12:03:37 +0100

    Implemented QuestionItem EdgarItem model

--- src/ApplicationImplementation/Edgar/Items/QuestionItem.ts
+++ src/ApplicationImplementation/Edgar/Items/QuestionItem.ts
@@ -1,4 +1,33 @@
+import { StudentParticipant } from "../Participants/StudentParticipant.js";
 import { EdgarItem } from "./EdgarItem.js";
 
 export class QuestionItem extends EdgarItem {
+    // questionId
+    // score
+    // maxScore
+    // scorePerc
+    // solvedByStudent || testInstanceRef
+
+    constructor(
+        id: number,
+
+        private readonly idStudent: number,
+
+        private readonly idTestInstance: number,
+
+        private readonly is_correct: boolean,
+        private readonly is_incorrect: boolean,
+        private readonly is_unanswered: boolean,
+        private readonly is_partial: boolean,
+
+        private readonly score: number,
+        maxScore: number,
+        private readonly score_perc: number,
+    ) {
+        super(id, maxScore);
+
+        this.participants.push(
+            new StudentParticipant(this, idStudent, score, score_perc)
+        );
+    }
 }
