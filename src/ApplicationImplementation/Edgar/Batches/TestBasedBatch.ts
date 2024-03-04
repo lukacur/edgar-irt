37f6331566b80462bb8e70910de8e34860b1f69c
lukacur <92858303+lukacur@users.noreply.github.com>
2024-03-04 09:54:06 +0100

    Implemented TestBasedBatch

--- src/ApplicationImplementation/Edgar/Batches/TestBasedBatch.ts
+++ src/ApplicationImplementation/Edgar/Batches/TestBasedBatch.ts
@@ -1,14 +1,61 @@
 import { AbstractBatch } from "../../../ApplicationModel/Batch/AbstractBatch.js";
-import { EdgarItem } from "../Items/EdgarItem.js";
+import { DatabaseConnection } from "../../Database/DatabaseConnection.js";
+import { TestInstance } from "../../Models/Database/TestInstance/TestInstance.model.js";
+import { TestInstanceBasedBatch } from "./TestInstanceBasedBatch.js";
 
-export class TestBasedBatch extends AbstractBatch<EdgarItem> {
-    loadItems(): Promise<EdgarItem[]> {
+export class TestBasedBatch extends AbstractBatch<TestInstanceBasedBatch> {
+    constructor(
+        private readonly databaseConnection: DatabaseConnection,
+
+        private readonly id: number,
+
+        private readonly idTestType: number,
+        private readonly idAcademicYear: number,
+        private readonly maxScore: number,
+
+        private readonly questionsNo: number,
+
+        private readonly title?: string,
+    ) {
+        super();
+    }
+
+    async loadItems(): Promise<TestInstanceBasedBatch[]> {
+        const queryResult =
+            await this.databaseConnection.doQuery<TestInstance & { manual_grade: number | null }>(
+                `SELECT test_instance.*
+                FROM test_instance
+                WHERE test_instance.id_test = $1`,
+                [this.id]
+            );
+
+        if (queryResult === null || queryResult.count === 0) {
+            this.items = [];
+        } else {
+            this.items = queryResult.rows
+                .map(ti =>
+                    new TestInstanceBasedBatch(
+                        this.databaseConnection,
+
+                        ti.id,
+
+                        this.idAcademicYear,
+                        this.id,
+                        ti.id_student,
+
+                        ti.score ?? 0,
+                        this.maxScore,
+                        ti.score_perc ?? 0,
+                    )
+                );
+        }
+
+        return this.items;
+    }
+    addItemToBatch(item: TestInstanceBasedBatch): Promise<AbstractBatch<TestInstanceBasedBatch>> {
         throw new Error("Method not implemented.");
     }
-    addItemToBatch(item: EdgarItem): Promise<AbstractBatch<EdgarItem>> {
-        throw new Error("Method not implemented.");
-    }
-    getLoadedItems(): EdgarItem[] | null {
+    getLoadedItems(): TestInstanceBasedBatch[] | null {
         throw new Error("Method not implemented.");
     }
     
