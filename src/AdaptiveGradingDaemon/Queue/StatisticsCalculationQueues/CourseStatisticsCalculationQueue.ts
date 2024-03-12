d45f64a441a40b4a45cc2dc55317ba1c3ebca6d2
lukacur <92858303+lukacur@users.noreply.github.com>
2024-03-12 13:01:33 +0100

    Added CourseStatisticsCalculationQueue queue implementation

---
+++ src/AdaptiveGradingDaemon/Queue/StatisticsCalculationQueues/CourseStatisticsCalculationQueue.ts
@@ -0,0 +1,37 @@
+import { IQueueSystemBase } from "../IQueueSystemBase.js";
+
+type CourseStatisticsProcessingRequest = {
+    idCourse: number;
+    idStartAcademicYear: number;
+    numberOfIncludedPreviousYears: number;
+
+    forceCalculation: boolean;
+}
+
+export class CourseStatisticsCalculationQueue implements IQueueSystemBase<CourseStatisticsProcessingRequest> {
+    constructor(
+        private readonly usedQueue: IQueueSystemBase<CourseStatisticsProcessingRequest>
+    ) {}
+
+    public async enqueue(data: CourseStatisticsProcessingRequest): Promise<boolean> {
+        return await this.usedQueue.enqueue(data);
+    }
+
+    public async dequeue(): Promise<CourseStatisticsProcessingRequest> {
+        return await this.usedQueue.dequeue();
+    }
+
+    public async peek(): Promise<CourseStatisticsProcessingRequest | null> {
+        return await this.usedQueue.peek();
+    }
+
+
+    public async empty(): Promise<boolean> {
+        return await this.usedQueue.empty();
+    }
+
+
+    public async close(): Promise<void> {
+        await this.usedQueue.close();
+    }
+}
