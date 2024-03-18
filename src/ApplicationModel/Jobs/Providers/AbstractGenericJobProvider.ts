cf8a4d21a69def7067b4d4b985c2b5726dcc4e47
lukacur <92858303+lukacur@users.noreply.github.com>
2024-03-18 15:15:06 +0100

    Added an abstract generic job provider model for type-safety

---
+++ src/ApplicationModel/Jobs/Providers/AbstractGenericJobProvider.ts
@@ -0,0 +1,15 @@
+import { IJobConfiguration } from "../IJobConfiguration.js";
+import { IJobProvider } from "./IJobProvider.js";
+
+export abstract class AbstractGenericJobProvider<
+    TJobConfiguration extends IJobConfiguration
+> implements IJobProvider {
+    protected abstract provideJobTyped(): Promise<TJobConfiguration>;
+
+    public async provideJob(): Promise<IJobConfiguration> {
+        return await this.provideJobTyped();
+    }
+
+    public abstract extendJob(extendForMs: number): Promise<"success" | "fail" | "job-inactive">;
+    public abstract finishJob(jobId: string): Promise<boolean>;
+}
