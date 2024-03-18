6e14947fb428619cb6cb90a52e891194db5b3c30
lukacur <92858303+lukacur@users.noreply.github.com>
2024-03-18 21:17:26 +0100

    Added a generic implementation of the AbstractJobWorker model

---
+++ src/ApplicationModel/Jobs/Workers/GenericJobWorker.ts
@@ -0,0 +1,20 @@
+import { IJobConfiguration } from "../IJobConfiguration.js";
+import { IJobStep } from "../IJobStep.js";
+import { AbstractJobWorker } from "./AbstractJobWorker.js";
+
+export class GenericJobWorker extends AbstractJobWorker<object, object> {
+    private executionResult: object | null = null;
+    constructor(
+        private readonly jobConfig: IJobConfiguration,
+    ) {
+        super();
+    }
+
+    protected override async executeStep(jobStep: IJobStep, stepInput: object | null): Promise<object | null> {
+        return (this.executionResult = await jobStep.run(stepInput));
+    }
+
+    protected override async getExecutionResultTyped(): Promise<object | null> {
+        return this.executionResult;
+    }
+}
