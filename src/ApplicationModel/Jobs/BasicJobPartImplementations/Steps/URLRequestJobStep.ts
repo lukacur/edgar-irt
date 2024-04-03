8791981082686b184faa315a4f14eb93ae799507
lukacur <92858303+lukacur@users.noreply.github.com>
2024-04-04 00:15:39 +0200

    Update URLRequestJobStep
    Fully implemented URL fetch logic

--- src/ApplicationModel/Jobs/BasicJobPartImplementations/Steps/URLRequestJobStep.ts
+++ src/ApplicationModel/Jobs/BasicJobPartImplementations/Steps/URLRequestJobStep.ts
@@ -1,41 +1,87 @@
+import { RegistryDefaultConstants } from "../../../../PluginSupport/RegistryDefault.constants.js";
+import { RegisterDelegateToRegistry } from "../../../Decorators/Registration.decorator.js";
 import { AbstractGenericJobStep } from "../../AbstractGenericJobStep.js";
+import { JobStepDescriptor } from "../../IJobConfiguration.js";
 import { StepResult } from "../../IJobStep.js";
+import { URLJobStepConfiguration } from "./Models/URLJobStepConfiguration.js";
 
-interface URLStepConfiguration {
-    readonly httpMethod: "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" | "PATCH";
-    readonly url: URL;
-
-    readonly body: BodyInit | null | undefined;
-    readonly headers: HeadersInit | undefined;
-}
+type URLFetchResultType = { result: string | object | Buffer };
 
 export class URLRequestJobStep
-    extends AbstractGenericJobStep<URLStepConfiguration, URLStepConfiguration, Response> {
-    public async runTyped(stepInput: (URLStepConfiguration | null)[]): Promise<StepResult<Response>> {
-        const stepIn = stepInput[0] ?? this.stepConfiguration;
+    extends AbstractGenericJobStep<URLJobStepConfiguration, Partial<URLJobStepConfiguration>, URLFetchResultType> {
+
+    protected async runTyped(
+        stepInput: (Partial<URLJobStepConfiguration> | null)[]
+    ): Promise<StepResult<URLFetchResultType>> {
+        const inputConfig = stepInput[0] ?? this.stepConfiguration;
+        const hdrs = (inputConfig.headers ?? this.stepConfiguration.headers ?? []);
+        hdrs.push(...this.stepConfiguration.appendedHeaders);
+
+        const headers = new Headers();
+        hdrs.forEach(hdr => { headers.append(hdr[0], hdr[1]); });
 
         try {
-            const result = await fetch(
-                stepIn.url,
+            const response = await fetch(
+                inputConfig.url ?? this.stepConfiguration.url,
                 {
-                    method: stepIn.httpMethod,
-                    body: stepIn.body,
-                    headers: stepIn.headers,
-                },
+                    method: inputConfig.httpMethod ?? this.stepConfiguration.httpMethod,
+                    body: inputConfig.body ?? this.stepConfiguration.body,
+                    headers,
+                }
             );
+
+            let responseDataResult: any = response;
+            switch (this.stepConfiguration.return) {
+                case "body-text": {
+                    responseDataResult = await response.text();
+                    break;
+                }
+
+                case "body-json": {
+                    responseDataResult = await response.json();
+                    break;
+                }
+
+                case "body-buffer": {
+                    responseDataResult = response.body;
+                    break;
+                }
+            }
 
             return {
                 status: "success",
-                result,
+                result: {
+                    result: responseDataResult,
+                },
+                resultTTLSteps: this.resultTTL,
             };
         } catch (err: any) {
-            console.log(err);
+            const msg = (err instanceof Error) ? 
+                `Error: ${err.name}
+Message: ${err.message}
+Stack trace: ${err.stack ?? "-"}` : err.toString();
+
             return {
                 status: "failure",
+                reason:
+                `Exception was thrown when calling 'fetch'
+Error information:
+${msg.split("\n").join("    \n")}`,
+                canRetry: false,
                 result: null,
-                reason: (typeof(err) === "string") ? err : ('toString' in err) ? err.toString() : "Unknown",
-                canRetry: true,
             };
         }
+    }
+    
+    @RegisterDelegateToRegistry(
+        "JobStep",
+        RegistryDefaultConstants.jobSteps.URL_INPUT_FETCH,
+    )
+    public create(stepDescriptor: JobStepDescriptor, ...args: any[]): object {
+        return new URLRequestJobStep(
+            stepDescriptor.stepTimeoutMs,
+            <URLJobStepConfiguration>stepDescriptor.configContent,
+            stepDescriptor.resultTTL,
+        );
     }
 }
--- src/PluginSupport/RegistryDefault.constants.ts
+++ src/PluginSupport/RegistryDefault.constants.ts
@@ -1,3 +1,7 @@
 export class RegistryDefaultConstants {
     public static readonly DEFAULT_DATABASE_CONNECTION_KEY = "DEFAULT_CONNECTION";
+
+    public static readonly jobSteps = {
+        URL_INPUT_FETCH: "URLInputFetch"
+    } as const;
 }
